92cab6ecd33d0894b9d8cb6bea790ebe17740cab
maxgrossman <m.grossman1813@gmail.com>
2017-04-21 11:14:23 -0400

    fixed my glob moran's i, because my code was wrong before

--- example.js
+++ example.js
@@ -4,5 +4,5 @@ var fs = require('fs')
 // Hey, the  below gives me global Moran's I. Neat!!
 console.log("Global Moran's I for Set of Points: ")
 console.log(
-  spatStats.Moran.Point(JSON.parse(fs.readFileSync('leyteCentroids.geojson')),'val')
+  spatStats.Moran.globPoint(JSON.parse(fs.readFileSync('data/leyte.geojson')),'val')
 )
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var distance = require("@turf/distance")
 var mathjs = require("mathjs")
-// for distance based weight matrix
-// -> http://www.stat.purdue.edu/~bacraig/SCS/Spatial%20Correlation%20new.doc
-// for adjacency based weiht matrix
-// ->
 /**
  * @param {FeatureCollection|MultPoint} geoJSON
  * @param {String} value
@@ -11,45 +7,81 @@ var mathjs = require("mathjs")
 
 function Moran() {};
 
-Moran.Point = function(geoJSON, value) {
+Moran.globPoint = function(geoJSON, value) {
 
   // Moran's I for Points. spatial weights are based on inverse distnace
   //
-  //    Steps:
-  //      1. Calculate Spatial Weight's Matrix
-  //      2. Standardize Dataset per mean & stdev
-  //      3. use matrix and standard dataset to get Moran's
+  //    Equation:
+  //      I = (N / ΣiΣjWij) x (ΣiΣjWij(Xi-Xmean)(Xj-Xmean) / Σi(Xi-Xbar)^2)
+  //      Where:
+  //         N = number of observations (points)
+  //         ΣiΣjWij = sum of a spatial weights matrix, where spatial weight
+  //                   here is defined by inverse distance
+  //                  (such that closer things have more influence)
+  //         ΣiΣjWij(Xi-Xmean)(Xj-Xmean) = sum of weighted cross product matrix.
+  //                                       A matrix where very i,j pair represents
+  //                                       an observation's deviation from the mean
+  //                                       mutiplied by one of its neighbors' Deviation
+  //                                       from the mean, weighted, or multiplied
+  //                                       by the spatial weight from obserbation j on i
+  //         Σi(Xi-Xbar)^2) = sum of squared devations from the mean.
+  //
+  //    Code Steps:
+  //      1. Calculate the Matricies and array we need
+  //        - Spatial Weigths Matrix
+  //        - Cross Product Matrix
+  //        - Squared Deviations Array
+  //      2. Calculate the first term (N/Spatial Weights Matrix)
+  //      3. Calculate the second term (Spatially Weigthed Cross Product)/Deviation Matrix
+  //      4. Caclulate Global Moran's I (first term * second term)
   //
   //
   // Step 1.
-  // generate matrix with rows representing inverse distances
-  // between each point d and all other points, including itself
-  // creates a matrix like :
-  // |00 .5 .1|
-  // |.5 00 .5|
-  // |.1 .5 00|
-  SpatWeightMatrix = []
+  // generate Matricies and Array
+  WeightedCrossProdMatrix = []
+  SpatialWeightMatrix = []
+  DeviationArray = []
+  mean = mathjs.mean(geoJSON.features.map(function(d){return d.properties[value]}))
+  // for each feature observation...
   geoJSON.features.forEach(function(d) {
-    row = []
+    rowWCPM = []
+    rowSWM = []
+    yIyBar = d.properties[value]-mean
+    // add the squared deviation to the DeviationArray
+    DeviationArray.push(mathjs.square(yIyBar))
+    // generate rows for the SpatialWeight & WeightedCrossProduct Matricies
     for(i=0;i<geoJSON.features.length;i++) {
-      if(d===geoJSON.features[i]){weightedInverseDist=0} else {
-        weightedInverseDist = 1/distance(d,geoJSON.features[i],'kilometers')
+      // if geoJSON.features[i] is d (Xi), add a 0!!
+      if(d===geoJSON.features[i]){
+        rowWCPM.push(0)
+        rowSWM.push(0)
+      } else {
+        // generate InverseDistance. Thanks turf.js! You're the best!
+        InverseDistance = 1/distance(d,geoJSON.features[i],'miles')
+        // generate Xi's neighbor, Xj's deviation, from the mean
+        yJyBar = geoJSON.features[i].properties[value]-mean
+        // make zIzJ, the cross product of Xi and it's neighbor Xj
+        zIzJ = yIyBar * yJyBar
+        // push zIzJ, weighted by InverseDistance, to Xi's WeightedCrossProdMatrix row
+        rowWCPM.push(zIzJ * InverseDistance)
+        // push Xj's InverseDistance to Xi to Xi's SpatialWeightMatrix row
+        rowSWM.push(InverseDistance)
       }
-      row.push(weightedInverseDist)
     }
-    SpatWeightMatrix.push(row)
+    // once we've found weighted cross prodcuts and inverse distnace for
+    // all neighbors, push Xi's rows to the matricies
+    WeightedCrossProdMatrix.push(rowWCPM)
+    SpatialWeightMatrix.push(rowSWM)
   })
 
-  SpatWeightMatrix = mathjs.matrix(SpatWeightMatrix)
-  // Step 2.
-  // normalize property values from d to z where z = (d-mean)/standardDeviation
-  valueArray = geoJSON.features.map(function(d){return d.properties[value]})
-  mean = mathjs.mean(valueArray)
-  stdev = mathjs.std(valueArray)
-  valueArray = valueArray.map(function(d){return (d-mean)/stdev})
-  // Step 3.
-  // return the matrix multiplication of the valueArray & (valueArray * spatialWeightMatrix)
-  return mathjs.multiply(valueArray,mathjs.multiply(valueArray,SpatWeightMatrix))
+  // calculate the first term
+  nOverSumSpatWeights = (geoJSON.features.length)/mathjs.sum(SpatialWeightMatrix)
+  // calculate the second term
+  wcpmOverSumDevArray = mathjs.sum(WeightedCrossProdMatrix)/mathjs.sum(DeviationArray)
+  // make that Moran's I!!
+  moranI = mathjs.multiply(nOverSumSpatWeights,wcpmOverSumDevArray)
+  // return Moran's I
+  return moranI
 }
 
 exports.Moran = Moran;
